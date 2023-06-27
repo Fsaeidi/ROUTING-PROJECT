a3b65f699aae0f76affee55e1e5ac3e4385c970b
Fsaeidi <129076603+Fsaeidi@users.noreply.github.com>
2023-06-27 20:32:03 +0330

    g

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,8 +5,7 @@ export default function Products() {
     return (
         <div>
             <h1>Products</h1>
-            
-            <Link to='/about'>About</Link>
+                        <Link to='/about'>About</Link>
             <Link to='/contact'>Contact</Link>
             <Link to='/products'>Products</Link>
         </div>
